b89d06b40b85acf0e1cd40f3a6c499d559014e1a
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-03-01 20:33:32 +0900

    fix: docker-composeでバインドするポート番号を固定 & configパッケージでそのポート番号をテスト用設定として扱う

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,24 +1,61 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/caarlos0/env/v7"
 )
 
+type RunMode string
+
+const (
+	ModeDebug   RunMode = "debug"
+	ModeRelease RunMode = "release"
+)
+
 type Config struct {
-	ServePort     int    `env:"TODO_SERVE_PORT" envDefault:"8181"`
-	PgSQLHost     string `env:"TODO_PGSQL_HOST" envDefault:"127.0.0.1"`
-	PgSQLPort     int    `env:"TODO_PGSQL_PORT" emvDefault:"5432"`
-	PgSQLUser     string `env:"TODO_PGSQL_USER" envDefault:"todoapp"`
-	PgSQLPasswd   string `env:"TODO_PGSQL_PASSWORD" envDefault:"todoapp"`
-	PgSQLDatabase string `env:"TODO_PGSQL_DATABASE" envDefault:"todoapp"`
-	RedisHost     string `env:"TODO_REDIS_HOST" envDefault:"127.0.0.1"`
-	RedisPort     int    `env:"TODO_REDIS_PORT" envDefault:"6379"`
+	RunMode   RunMode `env:"TODO_RUN_MODE"`
+	ServePort int     `env:"TODO_SERVE_PORT"`
+
+	PgSQLHost     string `env:"TODO_PGSQL_HOST"`
+	PgSQLPort     int    `env:"TODO_PGSQL_PORT" envDefault:"5432"`
+	PgSQLUser     string `env:"TODO_PGSQL_USER" envDefault:"todouser"`
+	PgSQLPasswd   string `env:"TODO_PGSQL_PASSWORD" envDefault:"todopass"`
+	PgSQLDatabase string `env:"TODO_PGSQL_DATABASE" envDefault:"tododb"`
+
+	RedisHost string `env:"TODO_REDIS_HOST"`
+	RedisPort int    `env:"TODO_REDIS_PORT" envDefault:"6379"`
 }
 
 func NewFromEnv() (*Config, error) {
 	cfg := &Config{}
-	if err := env.Parse(cfg); err != nil {
+	opt := env.Options{
+		RequiredIfNoDef: true,
+	}
+	if err := env.Parse(cfg, opt); err != nil {
 		return nil, err
 	}
+	switch cfg.RunMode {
+	case ModeDebug, ModeRelease:
+		break
+	default:
+		return nil, fmt.Errorf("NewFromEnv: Invalid run mode: %q", cfg.RunMode)
+	}
 	return cfg, nil
+}
+
+var testConfig = Config{}
+
+func ForTesting() *Config {
+	return &Config{
+		RunMode:       ModeDebug,
+		ServePort:     8181,
+		PgSQLHost:     "127.0.0.1",
+		PgSQLPort:     25432,
+		PgSQLUser:     "todouser",
+		PgSQLPasswd:   "todopass",
+		PgSQLDatabase: "tododb",
+		RedisHost:     "127.0.0.1",
+		RedisPort:     26379,
+	}
 }
