13f3184799b15adc42033f2d6a709cacfa62773c
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-03-04 10:35:53 +0900

    refactor: AccessTokenをAuthTokenへリネーム

--- backend/domain/u_user.go
+++ backend/domain/u_user.go
@@ -3,12 +3,13 @@ package domain
 import "time"
 
 type UserID uint64
-type AccessToken string
+type AuthToken string
 
 type UserUcase interface {
 	Store(ctx Ctx, email, passwd, displayName string) (User, error)
 	List(Ctx) ([]User, error)
-	IssueAuthToken(ctx Ctx, email, passwd string) (AccessToken, error)
+	IssueAuthToken(ctx Ctx, email, passwd string) (AuthToken, error)
+	ValidateAuthToken(Ctx, AuthToken) (UserID, error)
 }
 
 func (u *User) ApplyTimezone(loc *time.Location) {
