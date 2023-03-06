d844a8b9b75fa5326f5695f9019545f2dde4ca0d
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-03-06 11:16:53 +0900

    fix: JSONパース時のエラーメッセージを分かりやすく

--- backend/presenter/restapi/errors.go
+++ backend/presenter/restapi/errors.go
@@ -5,5 +5,4 @@ import "github.com/labstack/echo/v4"
 var (
 	Err400InvalidReqContentType = echo.NewHTTPError(400, "invalid Content-Type")
 	Err400EmptyReqBody          = echo.NewHTTPError(400, "empty body")
-	Err400UndecodableJSON       = echo.NewHTTPError(400, "undecodable JSON")
 )
